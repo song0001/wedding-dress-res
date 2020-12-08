b0fe72b6239c4811f5b50b99921b7d4c69370216
dd <1751919770@qq.com>
2020-12-08 23:07:37 +0800

    响应式

---
+++ js/common.js
@@ -0,0 +1,52 @@
+$(function() {
+    $(".box").on("click", function() {
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active").addClass("close");
+            $(".header-m .nav").fadeOut();
+            $('body').css({ "overflow-x": "auto", "overflow-y": "auto" });
+        } else {
+            $(this).removeClass("close").addClass("active");
+            $(".header-m .nav").fadeIn();
+            $('body').css({ "overflow-x": "hidden", "overflow-y": "hidden" });
+        }
+        //   模糊
+        if ($('.content-filter').hasClass('filter')) {
+            $('.content-filter').removeClass('filter')
+        } else {
+            $('.content-filter').addClass('filter')
+        }
+
+    });
+    // 滚动导航栏
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 20) {
+            $('.header-m,.header').css({
+                    "background": "#fbfbfb",
+                    'height': '60px',
+                    "position": "fixed",
+                    "boxShadow": "0 1px 10px rgba(0, 0, 0, 0.3)"
+                })
+                // $('.veronica,.header .nav').css({
+                //     'height': 60,
+
+
+            // })
+            // $('.header .nav').css({
+            //     'lineHeight': 60
+            // })
+
+        }
+        if ($(window).scrollTop() < 10) {
+            $('.header-m,.header').css({
+                'height': '80px',
+                "width": "100%",
+                "position": "relative"
+            })
+        }
+    })
+
+
+
+
+
+})
